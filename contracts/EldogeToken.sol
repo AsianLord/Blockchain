6c2f1572474ea5b711a8bee23fc431955b200bc9
AsianLord <eldrin.dimabuyu368@gmail.com>
2022-09-08 01:39:04 +0100

    2. Transfer tokens

--- contracts/EldogeToken.sol
+++ contracts/EldogeToken.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.5.16;
 
+//this contract is responsible for knowing where each token is
 contract EldogeToken {
-	// Constructor
-	// Sets the total number of tokens
-	// Read the total number of tokens
+	// Name
+	string public name = "Eldoge Token";
+	// Symbol
+	string public symbol = "ELDOGE";
+	// standard
+	string public standard = 'Eldoge Token v1.0';
 
 	// state variable that writes to storage(blockchain)
 	// so will be publicly visible
@@ -11,15 +15,50 @@ contract EldogeToken {
 	// solidity provides getter function already because of
 	// public declaration of state variable
 	uint256 public totalSupply;
+
+	// This event is something that the contract is going to admit that the consumer can subscribe to
+	event Transfer(
+		address indexed _from, // _from account is msg.sender
+		address indexed _to,
+		uint256 _value
+	);
 	
-	// constructor() public {
-	// 	totalSupply = 100000000;
-	// }
+	// takes the address of the owner and returns
+	// returns an unsigned integer - the balance of the address
+	// this function is primarily responsible for knowing who has each token
+	mapping(address => uint256) public balanceOf;
 
-
-	// Constructor
+ 	// Constructor
+	// Sets the total number of tokens
+	// Read the total number of tokens
 	constructor (uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+
 		// state variable that writes to storage(blockchain)
+		// value passed from the migrations file "1_deploy_contracts.js" is set as 
+		// the value of total supply
 		totalSupply = _initialSupply;
 	}
+
+	// Transfer
+	// address is a special data type in solidity that corresponds to the user's connection to the blockchain
+
+	function transfer(address _to, uint256 _value) public returns (bool success){
+		// Exception if account doesn't have enough
+		// require is special to solidity that says if the evaluation in () is true then continue execution
+		// and if false then stop execution and flag an error
+		require(balanceOf[msg.sender] >= _value);
+		
+		// Transfer the balance
+		// deducts from sender account
+		balanceOf[msg.sender] -= _value;
+		// adds the deducted value from sender acount to the receiving account
+		balanceOf[_to] += _value;
+
+		// Transfer event
+		emit Transfer(msg.sender, _to, _value); // msg.sender is the account that is calling this function
+
+		// Return a boolean
+		return true;
+	}	
 }
--- test/EldogeToken.js
+++ test/EldogeToken.js
@@ -1,13 +1,59 @@
 var EldogeToken = artifacts.require("./EldogeToken.sol");
 
 contract("EldogeToken", function(accounts){
+	var tokenInstance;
+	// name, symbol and standard attributes
+	it('initialises the contract with the correct values', function(){
+		return EldogeToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			return tokenInstance.name();
+		}).then(function(name){
+			assert.equal(name, 'Eldoge Token', 'has the correct name');
+			return tokenInstance.symbol();
+		}).then(function(symbol){
+			assert.equal(symbol, 'ELDOGE', 'has the correct symbol');
+			return tokenInstance.standard();
+		}).then(function(standard){
+			assert.equal(standard, 'Eldoge Token v1.0', 'has the correct standard');
+		});
+	});
 
-	it('sets the total suppy upon deployment', function(){
+	it('allocates the initial supply upon deployment', function(){
 		return EldogeToken.deployed().then(function(instance){
 			tokenInstance =  instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply){
 			assert.equal(totalSupply.toNumber(), 10000000, 'sets the total supply to 10,000,000');
+			return tokenInstance.balanceOf(accounts[0]); // accounts[0] first account indexed [0]
+		}).then(function(adminBalance){
+			assert.equal(adminBalance.toNumber(), 10000000, 'it allocates the initial supply to the admin');
+		});
+	});
+
+	//transfer function
+	it('transfers token ownership', function() {
+		return EldogeToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			//Test `require` statement first by transferring something larger than the sender's balance
+			return tokenInstance.transfer.call(accounts[1], 999999999999999);
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+			return tokenInstance.transfer.call(accounts[1], 2500000, { from : accounts[0] });
+		}).then(function(success){
+			assert.equal(success, true , 'it returns true'); //asserts that success is equal to true and returns true
+			return tokenInstance.transfer(accounts[1], 2500000, { from: accounts[0] });
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+			assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+			assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+			assert.equal(receipt.logs[0].args._value, 2500000, 'logs the transfer amount');
+			return tokenInstance.balanceOf(accounts[1]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 2500000, 'adds the amount to the receiving account');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 7500000, 'deducts the amount from the sending account');
 		});
 	});
 });
